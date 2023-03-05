794ebcefdb892033129a3c04e773c15e05f9311e
Ramin Samadi <ramin.samadi+github@elev.ga.ntig.se>
2023-03-05 16:49:05 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pacman -Syu git mpg123 python-pip
+pip install langdetect
+cd ~/ && git clone https://aur.archlinux.org/python-gtts.git && makepkg -sif --clean
+
+echo "
+function chatgpt() {
+  local answer=\$(sgpt --no-animation \"\$*\")
+  echo \"\$answer\"
+  language=\$(echo \"\$*\" | python -c \"from langdetect import detect; print(detect('\$*'))\" 2>/dev/null)
+  gtts-cli -l \"\$language\" \"\$answer\" --output tts.mp3
+  mpg123 -q ~/tts.mp3
+}
+" >> ~/.bashrc && source ~/.bashrc
